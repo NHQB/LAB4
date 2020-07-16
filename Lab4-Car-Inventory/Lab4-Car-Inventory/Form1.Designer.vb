b7cff1a323ede6ff48e8d9bf2e33f952afcb7726
NHQB <baon3183@gmail.com>
2020-07-15 20:20:53 -0400

    Initial Visual Studio project creation

---
+++ Lab4-Car-Inventory/Lab4-Car-Inventory/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMake = New System.Windows.Forms.Label()
+        Me.lblModel = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.lblNew = New System.Windows.Forms.Label()
+        Me.cbMake = New System.Windows.Forms.ComboBox()
+        Me.cbYear = New System.Windows.Forms.ComboBox()
+        Me.txtModel = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.ckNew = New System.Windows.Forms.CheckBox()
+        Me.lvCarInventory = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.txtError = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblMake
+        '
+        Me.lblMake.AutoSize = True
+        Me.lblMake.Location = New System.Drawing.Point(53, 12)
+        Me.lblMake.Name = "lblMake"
+        Me.lblMake.Size = New System.Drawing.Size(42, 17)
+        Me.lblMake.TabIndex = 0
+        Me.lblMake.Text = "Make"
+        '
+        'lblModel
+        '
+        Me.lblModel.AutoSize = True
+        Me.lblModel.Location = New System.Drawing.Point(53, 42)
+        Me.lblModel.Name = "lblModel"
+        Me.lblModel.Size = New System.Drawing.Size(46, 17)
+        Me.lblModel.TabIndex = 1
+        Me.lblModel.Text = "Model"
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Location = New System.Drawing.Point(53, 70)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(38, 17)
+        Me.lblYear.TabIndex = 2
+        Me.lblYear.Text = "Year"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(53, 100)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(40, 17)
+        Me.lblPrice.TabIndex = 3
+        Me.lblPrice.Text = "Price"
+        '
+        'lblNew
+        '
+        Me.lblNew.AutoSize = True
+        Me.lblNew.Location = New System.Drawing.Point(53, 128)
+        Me.lblNew.Name = "lblNew"
+        Me.lblNew.Size = New System.Drawing.Size(35, 17)
+        Me.lblNew.TabIndex = 4
+        Me.lblNew.Text = "New"
+        '
+        'cbMake
+        '
+        Me.cbMake.FormattingEnabled = True
+        Me.cbMake.Items.AddRange(New Object() {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"})
+        Me.cbMake.Location = New System.Drawing.Point(110, 12)
+        Me.cbMake.Name = "cbMake"
+        Me.cbMake.Size = New System.Drawing.Size(121, 24)
+        Me.cbMake.TabIndex = 5
+        '
+        'cbYear
+        '
+        Me.cbYear.FormattingEnabled = True
+        Me.cbYear.Items.AddRange(New Object() {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"})
+        Me.cbYear.Location = New System.Drawing.Point(110, 70)
+        Me.cbYear.Name = "cbYear"
+        Me.cbYear.Size = New System.Drawing.Size(121, 24)
+        Me.cbYear.TabIndex = 6
+        '
+        'txtModel
+        '
+        Me.txtModel.Location = New System.Drawing.Point(110, 42)
+        Me.txtModel.Name = "txtModel"
+        Me.txtModel.Size = New System.Drawing.Size(121, 22)
+        Me.txtModel.TabIndex = 7
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(110, 100)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(121, 22)
+        Me.txtPrice.TabIndex = 8
+        '
+        'ckNew
+        '
+        Me.ckNew.AutoSize = True
+        Me.ckNew.Location = New System.Drawing.Point(110, 128)
+        Me.ckNew.Name = "ckNew"
+        Me.ckNew.Size = New System.Drawing.Size(18, 17)
+        Me.ckNew.TabIndex = 9
+        Me.ckNew.UseVisualStyleBackColor = True
+        '
+        'lvCarInventory
+        '
+        Me.lvCarInventory.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.lvCarInventory.HideSelection = False
+        Me.lvCarInventory.Location = New System.Drawing.Point(12, 151)
+        Me.lvCarInventory.Name = "lvCarInventory"
+        Me.lvCarInventory.Size = New System.Drawing.Size(391, 211)
+        Me.lvCarInventory.TabIndex = 10
+        Me.lvCarInventory.UseCompatibleStateImageBehavior = False
+        Me.lvCarInventory.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "New"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "ID"
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Make"
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Model"
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Year"
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Price"
+        '
+        'txtError
+        '
+        Me.txtError.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtError.Location = New System.Drawing.Point(12, 368)
+        Me.txtError.Multiline = True
+        Me.txtError.Name = "txtError"
+        Me.txtError.ReadOnly = True
+        Me.txtError.Size = New System.Drawing.Size(391, 97)
+        Me.txtError.TabIndex = 11
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(328, 471)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(247, 471)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 13
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(166, 471)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 14
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(415, 503)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.txtError)
+        Me.Controls.Add(Me.lvCarInventory)
+        Me.Controls.Add(Me.ckNew)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtModel)
+        Me.Controls.Add(Me.cbYear)
+        Me.Controls.Add(Me.cbMake)
+        Me.Controls.Add(Me.lblNew)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.lblModel)
+        Me.Controls.Add(Me.lblMake)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Car Inventory"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMake As Label
+    Friend WithEvents lblModel As Label
+    Friend WithEvents lblYear As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents lblNew As Label
+    Friend WithEvents cbMake As ComboBox
+    Friend WithEvents cbYear As ComboBox
+    Friend WithEvents txtModel As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents ckNew As CheckBox
+    Friend WithEvents lvCarInventory As ListView
+    Friend WithEvents txtError As TextBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents ColumnHeader5 As ColumnHeader
+    Friend WithEvents ColumnHeader6 As ColumnHeader
+End Class
