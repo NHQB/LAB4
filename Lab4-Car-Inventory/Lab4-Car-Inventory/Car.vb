98a3ff15ed9a96bd6c131e28af381a39ef592b0a
NHQB <baon3183@gmail.com>
2020-07-16 13:49:45 -0400

    Class Car.vb creation

---
+++ Lab4-Car-Inventory/Lab4-Car-Inventory/Car.vb
@@ -0,0 +1,127 @@
+﻿Option Strict On
+Public Class Car
+
+#Region "Properties"
+    Private carMake As String = String.Empty
+    Private carModel As String = String.Empty
+    Private carYear As String = String.Empty
+    Private carPrice As String = String.Empty
+    Private carNew As Boolean = False
+    Private carInternalID As Integer = 0
+    Shared carCount As Integer = 0
+#End Region
+
+#Region "Constructors"
+    ''' <summary>
+    ''' Create a new car using default values
+    ''' </summary>
+    Public Sub New()
+        carInternalID = carCount
+        carCount += 1
+    End Sub
+
+    ''' <summary>
+    ''' Create a new car using provided values
+    ''' </summary>
+    ''' <param name="number">Student ID Number</param>
+    ''' <param name="name">Student Name</param>
+    ''' <param name="program">Student Program of Study</param>
+    ''' <param name="tuitionStatus">Student Tuition Payment Status</param>
+    Public Sub New(number As String, name As String, program As String, tuitionStatus As Boolean)
+        carInternalID = carCount
+        carCount += 1
+        Me.Make = Make
+        Me.Model = Model
+        Me.Year = Year
+        Me.Price = Price
+        Me.Neww = Neww
+    End Sub
+#End Region
+
+#Region "Property Methods"
+    ''' <summary>
+    ''' Get/Set the car make
+    ''' </summary>
+    ''' <returns>Car make as string</returns>
+    Public Property Make() As String
+        Get
+            Return carMake
+        End Get
+        Set(ByVal Value As String)
+            carMake = Value
+        End Set
+    End Property
+
+    ''' <summary>
+    '''  Get/Set the car model
+    ''' </summary>
+    ''' <returns>Car model as string</returns>
+    Public Property Model() As String
+        Get
+            Return carModel
+        End Get
+        Set(ByVal Value As String)
+            carModel = Value
+        End Set
+    End Property
+
+    ''' <summary>
+    '''  Get/Set the year of the car
+    ''' </summary>
+    ''' <returns>Year of the car as string</returns>
+    Public Property Year() As String
+        Get
+            Return carYear
+        End Get
+        Set(ByVal Value As String)
+            carYear = Value
+        End Set
+    End Property
+
+    ''' <summary>
+    '''  Get/Set the car price
+    ''' </summary>
+    ''' <returns>Car price as string</returns>
+    Public Property Price() As String
+        Get
+            Return carPrice
+        End Get
+        Set(ByVal Value As String)
+            carPrice = Value
+        End Set
+    End Property
+
+    ''' <summary>
+    '''  Get/Set whether the car is new
+    ''' </summary>
+    ''' <returns>New status as boolean</returns>
+    Public Property Neww() As Boolean
+        Get
+            Return carNew
+        End Get
+        Set(ByVal value As Boolean)
+            carNew = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' Get the car's internal ID 
+    ''' </summary>
+    ''' <returns>Cars internal ID as integer</returns>
+    Public ReadOnly Property ID() As Integer
+        Get
+            Return carInternalID
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Get the current count of cars
+    ''' </summary>
+    ''' <returns>Count of current cars as integer</returns>
+    Public ReadOnly Property Count() As Integer
+        Get
+            Return carCount
+        End Get
+    End Property
+#End Region
+End Class
--- Lab4-Car-Inventory/Lab4-Car-Inventory/Form1.Designer.vb
+++ Lab4-Car-Inventory/Lab4-Car-Inventory/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.lblMake = New System.Windows.Forms.Label()
         Me.lblModel = New System.Windows.Forms.Label()
         Me.lblYear = New System.Windows.Forms.Label()
@@ -33,16 +34,17 @@ Partial Class Form1
         Me.txtPrice = New System.Windows.Forms.TextBox()
         Me.ckNew = New System.Windows.Forms.CheckBox()
         Me.lvCarInventory = New System.Windows.Forms.ListView()
-        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.txtError = New System.Windows.Forms.TextBox()
+        Me.colNew = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colID = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colMake = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colModel = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colYear = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colPrice = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.txtOutput = New System.Windows.Forms.TextBox()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.btnReset = New System.Windows.Forms.Button()
         Me.btnEnter = New System.Windows.Forms.Button()
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
         Me.SuspendLayout()
         '
         'lblMake
@@ -50,63 +52,67 @@ Partial Class Form1
         Me.lblMake.AutoSize = True
         Me.lblMake.Location = New System.Drawing.Point(53, 12)
         Me.lblMake.Name = "lblMake"
-        Me.lblMake.Size = New System.Drawing.Size(42, 17)
+        Me.lblMake.Size = New System.Drawing.Size(46, 17)
         Me.lblMake.TabIndex = 0
-        Me.lblMake.Text = "Make"
+        Me.lblMake.Text = "&Make:"
         '
         'lblModel
         '
         Me.lblModel.AutoSize = True
         Me.lblModel.Location = New System.Drawing.Point(53, 42)
         Me.lblModel.Name = "lblModel"
-        Me.lblModel.Size = New System.Drawing.Size(46, 17)
+        Me.lblModel.Size = New System.Drawing.Size(50, 17)
         Me.lblModel.TabIndex = 1
-        Me.lblModel.Text = "Model"
+        Me.lblModel.Text = "M&odel:"
         '
         'lblYear
         '
         Me.lblYear.AutoSize = True
         Me.lblYear.Location = New System.Drawing.Point(53, 70)
         Me.lblYear.Name = "lblYear"
-        Me.lblYear.Size = New System.Drawing.Size(38, 17)
+        Me.lblYear.Size = New System.Drawing.Size(42, 17)
         Me.lblYear.TabIndex = 2
-        Me.lblYear.Text = "Year"
+        Me.lblYear.Text = "&Year:"
         '
         'lblPrice
         '
         Me.lblPrice.AutoSize = True
         Me.lblPrice.Location = New System.Drawing.Point(53, 100)
         Me.lblPrice.Name = "lblPrice"
-        Me.lblPrice.Size = New System.Drawing.Size(40, 17)
+        Me.lblPrice.Size = New System.Drawing.Size(44, 17)
         Me.lblPrice.TabIndex = 3
-        Me.lblPrice.Text = "Price"
+        Me.lblPrice.Text = "&Price:"
         '
         'lblNew
         '
         Me.lblNew.AutoSize = True
         Me.lblNew.Location = New System.Drawing.Point(53, 128)
         Me.lblNew.Name = "lblNew"
-        Me.lblNew.Size = New System.Drawing.Size(35, 17)
+        Me.lblNew.Size = New System.Drawing.Size(39, 17)
         Me.lblNew.TabIndex = 4
-        Me.lblNew.Text = "New"
+        Me.lblNew.Text = "&New:"
         '
         'cbMake
         '
+        Me.cbMake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbMake.FormattingEnabled = True
         Me.cbMake.Items.AddRange(New Object() {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"})
         Me.cbMake.Location = New System.Drawing.Point(110, 12)
         Me.cbMake.Name = "cbMake"
         Me.cbMake.Size = New System.Drawing.Size(121, 24)
         Me.cbMake.TabIndex = 5
+        Me.ToolTip.SetToolTip(Me.cbMake, "Select the car maker brand")
         '
         'cbYear
         '
+        Me.cbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbYear.FormattingEnabled = True
         Me.cbYear.Items.AddRange(New Object() {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"})
         Me.cbYear.Location = New System.Drawing.Point(110, 70)
         Me.cbYear.Name = "cbYear"
         Me.cbYear.Size = New System.Drawing.Size(121, 24)
         Me.cbYear.TabIndex = 6
+        Me.ToolTip.SetToolTip(Me.cbYear, "Select the year that the car is released")
         '
         'txtModel
         '
@@ -114,6 +120,7 @@ Partial Class Form1
         Me.txtModel.Name = "txtModel"
         Me.txtModel.Size = New System.Drawing.Size(121, 22)
         Me.txtModel.TabIndex = 7
+        Me.ToolTip.SetToolTip(Me.txtModel, "Type in the car model")
         '
         'txtPrice
         '
@@ -121,6 +128,7 @@ Partial Class Form1
         Me.txtPrice.Name = "txtPrice"
         Me.txtPrice.Size = New System.Drawing.Size(121, 22)
         Me.txtPrice.TabIndex = 8
+        Me.ToolTip.SetToolTip(Me.txtPrice, "Type in the price of the car")
         '
         'ckNew
         '
@@ -129,52 +137,58 @@ Partial Class Form1
         Me.ckNew.Name = "ckNew"
         Me.ckNew.Size = New System.Drawing.Size(18, 17)
         Me.ckNew.TabIndex = 9
+        Me.ToolTip.SetToolTip(Me.ckNew, "Is it a new car or not")
         Me.ckNew.UseVisualStyleBackColor = True
         '
         'lvCarInventory
         '
-        Me.lvCarInventory.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.lvCarInventory.CheckBoxes = True
+        Me.lvCarInventory.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.colNew, Me.colID, Me.colMake, Me.colModel, Me.colYear, Me.colPrice})
+        Me.lvCarInventory.FullRowSelect = True
         Me.lvCarInventory.HideSelection = False
         Me.lvCarInventory.Location = New System.Drawing.Point(12, 151)
+        Me.lvCarInventory.MultiSelect = False
         Me.lvCarInventory.Name = "lvCarInventory"
         Me.lvCarInventory.Size = New System.Drawing.Size(391, 211)
         Me.lvCarInventory.TabIndex = 10
+        Me.ToolTip.SetToolTip(Me.lvCarInventory, "Display the detail car models")
         Me.lvCarInventory.UseCompatibleStateImageBehavior = False
         Me.lvCarInventory.View = System.Windows.Forms.View.Details
         '
-        'ColumnHeader1
+        'colNew
         '
-        Me.ColumnHeader1.Text = "New"
+        Me.colNew.Text = "New"
         '
-        'ColumnHeader2
+        'colID
         '
-        Me.ColumnHeader2.Text = "ID"
+        Me.colID.Text = "ID"
         '
-        'ColumnHeader3
+        'colMake
         '
-        Me.ColumnHeader3.Text = "Make"
+        Me.colMake.Text = "Make"
         '
-        'ColumnHeader4
+        'colModel
         '
-        Me.ColumnHeader4.Text = "Model"
+        Me.colModel.Text = "Model"
         '
-        'ColumnHeader5
+        'colYear
         '
-        Me.ColumnHeader5.Text = "Year"
+        Me.colYear.Text = "Year"
         '
-        'ColumnHeader6
+        'colPrice
         '
-        Me.ColumnHeader6.Text = "Price"
+        Me.colPrice.Text = "Price"
         '
-        'txtError
+        'txtOutput
         '
-        Me.txtError.Cursor = System.Windows.Forms.Cursors.No
-        Me.txtError.Location = New System.Drawing.Point(12, 368)
-        Me.txtError.Multiline = True
-        Me.txtError.Name = "txtError"
-        Me.txtError.ReadOnly = True
-        Me.txtError.Size = New System.Drawing.Size(391, 97)
-        Me.txtError.TabIndex = 11
+        Me.txtOutput.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtOutput.Location = New System.Drawing.Point(12, 368)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(391, 97)
+        Me.txtOutput.TabIndex = 11
+        Me.ToolTip.SetToolTip(Me.txtOutput, "Display error")
         '
         'btnExit
         '
@@ -182,16 +196,19 @@ Partial Class Form1
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(75, 23)
         Me.btnExit.TabIndex = 12
-        Me.btnExit.Text = "Exit"
+        Me.btnExit.Text = "E&xit"
+        Me.ToolTip.SetToolTip(Me.btnExit, "Button to exit the form ")
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'btnReset
         '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnReset.Location = New System.Drawing.Point(247, 471)
         Me.btnReset.Name = "btnReset"
         Me.btnReset.Size = New System.Drawing.Size(75, 23)
         Me.btnReset.TabIndex = 13
-        Me.btnReset.Text = "Reset"
+        Me.btnReset.Text = "&Reset"
+        Me.ToolTip.SetToolTip(Me.btnReset, "Button to reset the  form")
         Me.btnReset.UseVisualStyleBackColor = True
         '
         'btnEnter
@@ -200,18 +217,21 @@ Partial Class Form1
         Me.btnEnter.Name = "btnEnter"
         Me.btnEnter.Size = New System.Drawing.Size(75, 23)
         Me.btnEnter.TabIndex = 14
-        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.Text = "&Enter"
+        Me.ToolTip.SetToolTip(Me.btnEnter, "Button to enter the information")
         Me.btnEnter.UseVisualStyleBackColor = True
         '
         'Form1
         '
+        Me.AcceptButton = Me.btnEnter
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
         Me.ClientSize = New System.Drawing.Size(415, 503)
         Me.Controls.Add(Me.btnEnter)
         Me.Controls.Add(Me.btnReset)
         Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.txtError)
+        Me.Controls.Add(Me.txtOutput)
         Me.Controls.Add(Me.lvCarInventory)
         Me.Controls.Add(Me.ckNew)
         Me.Controls.Add(Me.txtPrice)
@@ -243,14 +263,15 @@ Partial Class Form1
     Friend WithEvents txtPrice As TextBox
     Friend WithEvents ckNew As CheckBox
     Friend WithEvents lvCarInventory As ListView
-    Friend WithEvents txtError As TextBox
+    Friend WithEvents txtOutput As TextBox
     Friend WithEvents btnExit As Button
     Friend WithEvents btnReset As Button
     Friend WithEvents btnEnter As Button
-    Friend WithEvents ColumnHeader1 As ColumnHeader
-    Friend WithEvents ColumnHeader2 As ColumnHeader
-    Friend WithEvents ColumnHeader3 As ColumnHeader
-    Friend WithEvents ColumnHeader4 As ColumnHeader
-    Friend WithEvents ColumnHeader5 As ColumnHeader
-    Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents colNew As ColumnHeader
+    Friend WithEvents colID As ColumnHeader
+    Friend WithEvents colMake As ColumnHeader
+    Friend WithEvents colModel As ColumnHeader
+    Friend WithEvents colYear As ColumnHeader
+    Friend WithEvents colPrice As ColumnHeader
+    Friend WithEvents ToolTip As ToolTip
 End Class
